dec600ddf19453240149fd1ddaa6639535c89396
ZHAO Xudong <zxdong@gmail.com>
2019-05-19 23:54:10 +0800

    Add index

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const skill1 = require('./skill-ping-pong')
+const skill2 = require('./skill-simple-math')
+const skill3 = require('./skill-time')
+
+module.exports = exports.default = [
+  skill1, skill2, skill3
+]
